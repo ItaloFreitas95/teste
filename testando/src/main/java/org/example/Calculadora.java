8d47f46364f5f2dae6b736e961573d66fc035a10
ÍtaloFreitas95 <italorodriguesdefreitas95@gmail.com>
2023-10-31 19:25:45 -0300

    testes básicos Junit

---
+++ testando/src/main/java/org/example/Calculadora.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Calculadora {
+    private double a, b;
+
+    public Calculadora(double a, double b) {
+        this.a = b;
+        this.b = a;
+    }
+
+    public double soma() {
+        return this.a + this.b;
+    }
+}
